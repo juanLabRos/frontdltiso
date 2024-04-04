2118beae6a09842f23d8c39a4cca1b88624c785e
javierdm01 <diazmolinajavier04@gmail.com>
2024-04-04 09:27:46 +0200

    Routes

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashBoard() {
+    return(
+        <h1>Esto es dashboard</h1>
+    )
+}
---
+++ app/dashboard/premium/page.tsx
@@ -0,0 +1,5 @@
+export default function Premium() {
+    return ( 
+        <h1>Esto es premium</h1>
+    )
+}
