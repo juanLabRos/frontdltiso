6695d154b305544f2591c4a5b3e86d289dd8d81a
javierdm01 <diazmolinajavier04@gmail.com>
2024-04-05 08:27:52 +0200

    Fix button error

--- app/ui/ButtonCustom.tsx
+++ app/ui/ButtonCustom.tsx
@@ -1,11 +1,12 @@
-export default function ButtonCustom({children, color='customTeal',tono='',action='',textColor='white'}:{children:any,color?:string,tono?:any,action?:any,textColor?:string}){
+export default function ButtonCustom({ children, color = 'customTeal', tono = 400, action = '', textColor = 'white' }: { children: any, color?: string, tono?: number, action?: any, textColor?: string }) {
+    const bgColor = color === 'customTeal' ? `bg-${color}-dark` : `bg-${color}-${tono}`;
+    const hoverBgColor = color === 'customTeal' ? `bg-${color}-semidark` : `bg-${color}-${Number(tono)-100}`;
+    const focusOutlineColor = color === 'customTeal' ? `outline-${color}-semidark` : `outline-${color}-${Number(tono)-100}`;
+  
     return (
-        <a onClick={action} className={`flex w-full items-center justify-center rounded-md bg-${color}-${color =='customTeal' ?'dark':tono}  px-3 py-2 
-        text-sm font-semibold leading-6 text-${textColor} shadow-sm hover:bg-${color}-${color =='customTeal' ?'semidark':tono-100} focus-visible:outline focus-visible:outline-2 
-        focus-visible:outline-offset-2 focus-visible:outline-${color}-${color =='customTeal' ?'semidark':tono-100}`}>
+      <a onClick={action} className={`flex w-full items-center justify-center rounded-md ${bgColor} px-3 py-2 text-sm font-semibold leading-6 text-${textColor} hover:${hoverBgColor} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:${focusOutlineColor}`}>
         {children}
-        </a>
-        
-        
-    )
-}
+      </a>
+    );
+  }
+  
--- app/ui/Login.tsx
+++ app/ui/Login.tsx
@@ -5,13 +5,13 @@ import Image from 'next/image'
 export default function Login(){
     return (
         <div className=" md:w-4/6 max-w-4xl lg:w-1/2 flex flex-col gap-3 items-center bg-black bg-opacity-50 md:bg-transparent rounded-xl md:rounded-none px-4 py-3 lg:py-6 ">
-              <h2 className="mt-2 md:mt-6 text-center text-3xl tracking-wider font-bold leading-9 text-white">
+              <h2 className="mt-2 md:mt-5 text-center text-3xl tracking-wider font-bold leading-9 text-white">
                 Inicio Sesión
               </h2>
               <h4 className='text-center text-white'>Introduce tu email para iniciar sesión en la página</h4>
-              <div className="mt-3 md:mt-6 w-10/12 max-w-md">
+              <div className="mt-3 md:mt-4 w-9/12 max-w-md">
                 {/* --- FORMULARIO ---- */}
-              <form className="space-y-6" action="#" method="POST">
+              <form className="space-y-5" action="#" method="POST">
                   {/* --- EMAIL ---- */}
                   <InputForm label='Correo Electrónico' name='email' placeholder='Introduzca su correo electronico' type='email' key={'email'}/>
                     {/* --- CONTRASEÑA ---- */}
@@ -41,12 +41,19 @@ export default function Login(){
                   <ButtonCustom>
                     Iniciar sesión
                   </ButtonCustom>
+                  
+                </div>
+                {/* --- BTN REGISTRARSE ---- */}
+                <div className="mt-1">
+                  <ButtonCustom>
+                    Registrarse
+                  </ButtonCustom>
                 </div>
               </form>
             </div>
 
             {/* --- AUTENTICACIÓNES ---- */}
-            <div className="mt-2 md:mt-6 w-full md:w-9/12">
+            <div className="mt-1 md:mt-4 w-full md:w-9/12">
               {/* --- CONTINUAR ---- */}
               <div className="flex justify-between">
                 <div className="w-1/3 border-t border-gray-200 mt-3" aria-hidden="true">
@@ -58,7 +65,7 @@ export default function Login(){
                 </div>
               </div>
               {/* --- BOTONES ---- */}
-              <div className="mt-2 flex 2xl:flex-col gap-4 flex-row">
+              <div className="mt-1 flex 2xl:flex-col gap-4 flex-row">
                   {/* --- GOOGLE  ---- */}
                   <ButtonCustom color="gray" tono={200} textColor="black">
                     <Image src="./google.svg" width={20} height={20} alt="Google" className="w-5 mr-2 h-5"/>
