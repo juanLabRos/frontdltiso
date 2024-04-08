91af37e6e332f43f6021e7381cb0efa529186098
javierdm01 <diazmolinajavier04@gmail.com>
2024-04-08 10:17:31 +0200

    Refresh Felipe Premium

---
+++ app/ui/PremiumData.tsx
@@ -0,0 +1,9 @@
+
+
+export default function PremiumData({children}:{children:string}){
+    return (
+        <p className="text-black px-8 py-3 bg-[#F0F3F4] w-[360px]">
+              {children}
+        </p>
+    )
+}
